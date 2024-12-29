284665fe75fe87e3623782e53d19b0d318d646d0
Jeremy O'Brien <neutral@fastmail.com>
2024-12-29 14:06:55 -0500

    add chromium

---
+++ configs/chromium.nix
@@ -0,0 +1,20 @@
+{ ... }: {
+  programs.chromium = {
+    enable = true;
+    extraOpts = {
+      "BrowserSignin" = 0;
+      "SyncDisabled" = true;
+      "PasswordManagerEnabled" = false;
+      "SpellcheckEnabled" = true;
+      "SpellcheckLanguage" = [
+        "en-US"
+      ];
+      "RestoreOnStartup" = 1; # open last session
+    };
+    extensions = [
+      "cjpalhdlnbpafiamejdnhcphjbkeiagm" # ublock origin
+      "nngceckbapebfimnlniiiahkandclblb" # bitwarden
+      "nlffgllnjjkheddehpolbanogdeaogbc" # backspace to go back
+    ];
+  };
+}
--- configs/default.nix
+++ configs/default.nix
@@ -1,5 +1,6 @@
 { ... }: {
   imports = [
+    ./chromium.nix
     ./git.nix
     ./helix.nix
     ./jj.nix
