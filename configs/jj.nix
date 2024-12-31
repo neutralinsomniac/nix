a70b2c7972393cc3fad48527643b64dda06d656d
Jeremy O'Brien <neutral@fastmail.com>
2024-12-31 16:49:46 -0500

    use writeText for jj config

--- configs/jj.nix
+++ configs/jj.nix
@@ -3,36 +3,35 @@
 , ...
 }:
 let
-  tomlFmt = pkgs.formats.toml { };
   jjPkg = pkgsUnstable.jujutsu;
 
-  jjConfig = tomlFmt.generate "config.toml" {
-    user = {
-      name = "Jeremy O'Brien";
-      email = "neutral@fastmail.com";
-    };
-    ui = {
-      default-command = "log";
-      diff.format = "git";
-      pager = ":builtin";
-    };
-    alases = {
-      shortlog = ''["log", "-n", "20"]'';
-      up = ''["rebase", "-b", "@", "-d", "trunk()"]'';
-      a = ''["log", "-r", "all()"]'';
-      b = ''["bookmark"]'';
-      arst = ''["bookmark", "move", "--from", "heads(::@- & bookmarks())", "--to", "@-"]'';
-    };
-    templates.draft_commit_description = ''
-      concat(
-        description,
-        surround(
-          "\nJJ: This commit contains the following changes:\n", "",
-          indent("JJ:     ", diff.stat(72)),
-        ),
-      )
+  jjConfig = pkgs.writeText "config.toml"
+    ''
+    [user]
+    name = "Jeremy O'Brien"
+    email = "neutral@fastmail.com"
+
+    [ui]
+    default-command = "log"
+    diff.format = "git"
+
+    [aliases]
+    shortlog = ["log", "-n", "20"]
+    up = ["rebase", "-b", "@", "-d", "trunk()"]
+    a = ["log", "-r", "all()"]
+    b = ["bookmark"]
+    arst = ["bookmark", "move", "--from", "heads(::@- & bookmarks())", "--to", "@-"]
+
+    [templates]
+    draft_commit_description = """
+    concat(
+      description,
+      surround(
+        "\nJJ: This commit contains the following changes:\n", "",
+        indent("JJ:     ", diff.stat(72)),
+      ),
+    )"""
     '';
-  };
 
   xdgDir = pkgs.linkFarm "jj-config" [
     {
