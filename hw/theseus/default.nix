3fdaeee26968d4d6dcb87393e9bef1e37ec25b2b
Jeremy O'Brien <neutral@fastmail.com>
2025-01-06 13:29:00 -0500

    theseus: turn off audio enhancement; it messes with headphone output

--- hw/theseus/default.nix
+++ hw/theseus/default.nix
@@ -7,7 +7,7 @@
   ];
   config = {
     hardware.framework.amd-7040.preventWakeOnAC = true;
-    hardware.framework.laptop13.audioEnhancement.enable = true;
+    # hardware.framework.laptop13.audioEnhancement.enable = true;
     hardware.bluetooth.enable = true;
   };
 }
