e6d2ddeb02033baa6b9a48c8e8dca42f9e6f0e62
Jeremy O'Brien <neutral@fastmail.com>
2025-02-07 12:44:32 -0500

    enable nix-ld

--- configuration.nix
+++ configuration.nix
@@ -123,6 +123,12 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
+  programs.nix-ld.enable = true;
+  programs.nix-ld.libraries = with pkgs; [
+    # Add any missing dynamic libraries for unpackaged programs
+    # here, NOT in environment.systemPackages
+  ];
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
