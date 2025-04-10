0d70b7e37fbf92afcd74dc2e36fcd6036e9699cf
Jeremy O'Brien <neutral@fastmail.com>
2025-04-10 14:22:12 -0400

    add check-restart script

---
+++ check-restart.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use feature 'say';
+
+my @booted = split("/", `readlink -f /run/booted-system/kernel`);
+my @current = split("/", `readlink -f /run/current-system/kernel`);
+
+if ($booted[3] ne $current[3]) {
+	say "Restart required!";
+	say "old: $booted[3]";
+	say "new: $current[3]";
+      exit 1;
+} else {
+	say "system is clean..";
+}
