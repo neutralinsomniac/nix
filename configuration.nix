86cda5fb0bbc9f307a39ba5f50e9e21398a4c4ef
Jeremy O'Brien <neutral@fastmail.com>
2025-05-27 11:25:28 -0400

    enable ng rebuild

--- configuration.nix
+++ configuration.nix
@@ -221,6 +221,8 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.11"; # Did you read the comment?
 
+  system.rebuild.enableNg = true;
+
   nix.settings.experimental-features = [ "nix-command" "flakes"];
   # nix.settings.auto-optimise-store = true;
   # nix.gc = {
