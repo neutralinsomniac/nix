f5912a765045ddcb81c2f6fe1b8f2a0e601f63f3
Jeremy O'Brien <neutral@fastmail.com>
2025-05-27 11:25:28 -0400

    add gnome tweaks to gnome config

--- wm/gnome.nix
+++ wm/gnome.nix
@@ -1,6 +1,12 @@
+{ pkgs
+, ...
+}:
 {
   # Enable the GNOME Desktop Environment.
   services.xserver.enable = true;
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
+  environment.systemPackages = [
+    pkgs.gnome-tweaks
+  ];
 }
