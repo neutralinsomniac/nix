eddba1f8ea8feb5c631fbcb66aae8b31370f1815
Jeremy O'Brien <neutral@fastmail.com>
2025-06-02 10:40:11 -0400

    use sddm with cosmic

--- wm/cosmic.nix
+++ wm/cosmic.nix
@@ -1,4 +1,6 @@
 {
-  services.displayManager.cosmic-greeter.enable = true;
+  # services.displayManager.cosmic-greeter.enable = true;
+  services.displayManager.sddm.enable = true;
+  services.displayManager.sddm.wayland.enable = true;
   services.desktopManager.cosmic.enable = true;
 }
