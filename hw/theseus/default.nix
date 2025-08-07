5f721685bd36fb0627f538188694451e8b89f3b7
Jeremy O'Brien <neutral@fastmail.com>
2025-08-07 15:26:40 -0400

    theseus: switch to newer kernel in hopes of fixing amdgpu resume bug

--- hw/theseus/default.nix
+++ hw/theseus/default.nix
@@ -1,17 +1,20 @@
-{ inputs, ... }:
+{
+  lib,
+  pkgs,
+  inputs,
+  ...
+}:
 
 {
   imports = [
     ./hardware-configuration.nix
-    # ../../configs/renoise.nix
     inputs.nixos-hardware.nixosModules.framework-13-7040-amd
   ];
   config = {
-    hardware.framework.amd-7040.preventWakeOnAC = true;
-    # hardware.framework.enableKmod = true;
     # hardware.framework.laptop13.audioEnhancement.enable = true;
     hardware.bluetooth.enable = true;
     # until the sddm issue gets fixed
     services.fprintd.enable = false;
+    boot.kernelPackages = lib.mkIf (lib.versionOlder pkgs.linux.version "6.15") pkgs.linuxPackages_latest;
   };
 }
