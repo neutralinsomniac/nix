c1e161c0ec319f21131de06d661517b5dfc3c332
Jeremy O'Brien <neutral@fastmail.com>
2025-08-11 08:19:14 -0400

    turn back on preventWakeOnAC for framework

--- hw/theseus/default.nix
+++ hw/theseus/default.nix
@@ -11,6 +11,7 @@
     inputs.nixos-hardware.nixosModules.framework-13-7040-amd
   ];
   config = {
+    hardware.framework.amd-7040.preventWakeOnAC = true;
     # hardware.framework.laptop13.audioEnhancement.enable = true;
     hardware.bluetooth.enable = true;
     # until the sddm issue gets fixed
