ed324b5b9ab8d79a00cdfec74aac0aec18372f73
Jeremy O'Brien <neutral@fastmail.com>
2025-08-27 14:11:14 -0400

    limit number of boot entries to 5

--- configuration.nix
+++ configuration.nix
@@ -20,6 +20,7 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot.configurationLimit = 5;
 
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
