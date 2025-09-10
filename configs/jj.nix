3ee1e8bd78ed60fb089496133d5d1b678c2884ff
Jeremy O'Brien <neutral@fastmail.com>
2025-09-10 14:31:35 -0400

    jj: use unstable nixpkgs

--- configs/jj.nix
+++ configs/jj.nix
@@ -1,13 +1,13 @@
 {
   pkgs,
-  inputs,
-  # , pkgsUnstable
+  # inputs,
+  pkgsUnstable,
   ...
 }:
 let
   # jjPkg = pkgs.jujutsu;
-  # jjPkg = pkgsUnstable.jujutsu;
-  jjPkg = inputs.jujutsu.packages.x86_64-linux.default;
+  jjPkg = pkgsUnstable.jujutsu;
+  # jjPkg = inputs.jujutsu.packages.x86_64-linux.default;
 
   jjConfig = pkgs.writeText "config.toml" ''
     [user]
