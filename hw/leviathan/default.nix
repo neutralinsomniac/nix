e00b0a8f8ee157d70a5c2a106318887f91982dd3
Jeremy O'Brien <neutral@fastmail.com>
2025-09-18 20:37:23 -0400

    leviathan: blacklist the hid_asus module. it does weird things and isn't necessary

--- hw/leviathan/default.nix
+++ hw/leviathan/default.nix
@@ -13,6 +13,9 @@
     hardware.asus.flow.gv302x.amdgpu.psr.enable = false;
     boot.kernelParams = [ "amdgpu.dcdebugmask=0x600" ];
 
+    # this driver is buggy as heck
+    boot.blacklistedKernelModules = [ "hid_asus" ];
+
     hardware.bluetooth.enable = true;
   };
 }
