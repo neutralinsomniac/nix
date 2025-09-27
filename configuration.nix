e3b12ccfbb2196c262b53f6886c5cc62e69e66b2
Jeremy O'Brien <neutral@fastmail.com>
2025-09-27 13:19:31 -0400

    make avahi actually do what I want

--- configuration.nix
+++ configuration.nix
@@ -74,7 +74,14 @@
   services.avahi = {
     enable = true;
     nssmdns4 = true;
-    openFirewall = true;
+    publish = {
+      enable = true;
+      addresses = true;
+      domain = true;
+      hinfo = true;
+      userServices = true;
+      workstation = true;
+    };
   };
 
   # Enable sound with pipewire.
