95eedcc14f4a9be78b3de70e2c2968095db436a6
Jeremy O'Brien <neutral@fastmail.com>
2025-10-24 15:42:35 -0400

    modify gnome a bit

--- wm/gnome.nix
+++ wm/gnome.nix
@@ -1,5 +1,6 @@
 {
   pkgs,
+  pkgsUnstable,
   ...
 }:
 {
@@ -7,7 +8,15 @@
   services.xserver.enable = true;
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
-  environment.systemPackages = [
-    pkgs.gnome-tweaks
+  environment.systemPackages = with pkgs; [
+    gnome-tweaks
+    gnome-terminal
+    gnomeExtensions.appindicator
+    gnome-themes-extra
+    material-cursors
+    pkgsUnstable.catppuccin-cursors.mochaDark
+  ];
+  services.udev.packages = [
+    pkgs.gnome-settings-daemon
   ];
 }
