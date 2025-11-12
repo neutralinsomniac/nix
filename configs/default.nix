d973a1b9ae87f1cd5a02a924d42ef131925354a6
Jeremy O'Brien <neutral@fastmail.com>
2025-11-12 07:57:50 -0500

    use lib.optionals for arch-specific packages

--- configs/default.nix
+++ configs/default.nix
@@ -1,23 +1,19 @@
 {
   imports = [
     ./chromium.nix
-    ./discord.nix
     ./distrobox.nix
     ./firefox.nix
     ./ghostty.nix
     ./git.nix
     ./helix.nix
-    ./ida.nix
     ./jj.nix
     ./logitech.nix
     ./nh.nix
     # ./openra.nix
     ./pidgin.nix
     # ./renoise.nix
-    ./spotify.nix
     ./ssh.nix
     ./steam.nix
-    ./tidal.nix
     ./syncthing.nix
     ./tailscale.nix
     ./udev.nix
--- configuration.nix
+++ configuration.nix
@@ -209,6 +209,17 @@
     wireshark
     wl-clipboard
     yt-dlp
+  ] ++ lib.optionals (pkgs.stdenv.hostPlatform.system == "x86_64-linux") [
+    discord
+    tidal-hifi
+    spotify
+    (pkgs.callPackage pkgs.ida-pro {
+      # Alternatively, fetch the installer through `fetchurl`, use a local path, etc.
+      runfile = pkgs.fetchurl {
+        url = "https://pintobyte.com/tmp/ida-pro_92_x64linux.run";
+        hash = "sha256-qt0PiulyuE+U8ql0g0q/FhnzvZM7O02CdfnFAAjQWuE=";
+      };
+    })
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
