cc339f96732b4ca8bba89107f0024898bb57cfe6
Jeremy O'Brien <neutral@fastmail.com>
2026-01-10 11:26:24 -0500

    reverse highlight kanto

--- configs/tintin.nix
+++ configs/tintin.nix
@@ -8,7 +8,7 @@ let
 
   ttconfig = pkgs.writeText "tt.rc" ''
     #split
-    #high {%*Kanto%*} {reverse light red}
+    #high {%*Kanto%*} {reverse}
     #prompt {[%d/%d %d/%d %d/%d %d]} {[<019>%1/%2 <049>%3/%4 <029>%5/%6 <059>%7<079>]$ } {-1} {0}
     #session aard aardmud.org 4000
   '';
