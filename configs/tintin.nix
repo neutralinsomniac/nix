9b8bf640b17788ba664f1849a7079fdd88f3b5b2
Jeremy O'Brien <neutral@fastmail.com>
2026-01-10 22:13:59 -0500

    update tintin prompt

--- configs/tintin.nix
+++ configs/tintin.nix
@@ -9,7 +9,7 @@ let
   ttconfig = pkgs.writeText "tt.rc" ''
     #split
     #high {%*Kanto%*} {reverse}
-    #prompt {[%d/%d %d/%d %d/%d %d]} {[<019>%1/%2 <049>%3/%4 <029>%5/%6 <059>%7<079>]$ } {-1} {0}
+    #prompt {%*[%d/%d %d/%d %d/%d %d]} {%1[<019>%2/%3 <049>%4/%5 <029>%6/%7 <059>%8<079>]$ } {-1} {0}
     #session aard aardmud.org 4000
   '';
 
