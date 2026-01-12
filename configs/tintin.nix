dc3d126e7963b0b498e6b9225c83436dd9d1fc6a
Jeremy O'Brien <neutral@fastmail.com>
2026-01-11 21:57:49 -0500

    tintin: update daily blessing color

--- configs/tintin.nix
+++ configs/tintin.nix
@@ -9,7 +9,7 @@ let
   ttconfig = pkgs.writeText "tt.rc" ''
     #split
     #high {%*Kanto%*} {reverse}
-    #prompt {%*[%d/%d %d/%d %d/%d %d]} {%1[<019>%2/%3 <049>%4/%5 <029>%6/%7 <059>%8<079>]$ } {-1} {0}
+    #prompt {%*[%d/%d %d/%d %d/%d %d]} {<788>%1<079>[<019>%2/%3 <049>%4/%5 <029>%6/%7 <059>%8<079>]$ } {-1} {0}
     #session aard aardmud.org 4000
   '';
 
