cd8622d97dbb3928fe773c700078b93388f98f04
Jeremy O'Brien <neutral@fastmail.com>
2026-01-24 16:35:08 -0500

    add flashing tools to reticulum

--- configs/reticulum.nix
+++ configs/reticulum.nix
@@ -11,6 +11,11 @@
   environment.systemPackages = [
     pkgs.python3Packages.nomadnet
     pkgs.python3Packages.rns
+    pkgs.adafruit-nrfutil
+  ];
+
+  nixpkgs.config.permittedInsecurePackages = [
+    "python3.13-ecdsa-0.19.1"
   ];
 
   nixpkgs.overlays = [
