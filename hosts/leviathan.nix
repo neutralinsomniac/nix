4990c91f06ceff953f111e64bd102fdf125187fe
Jeremy O'Brien <neutral@fastmail.com>
2026-05-08 11:34:34 -0400

    leviathan: remove redundant configs

--- hosts/leviathan.nix
+++ hosts/leviathan.nix
@@ -4,17 +4,12 @@
     name = "leviathan";
     myHidpiScale = 1.75;
     extraModules = [
-      inputs.nixos-hardware.nixosModules.asus-flow-gv302x-amdgpu
+      inputs.nixos-hardware.nixosModules.asus-flow-gz302ea
       (inputs.self + "/hw/leviathan/disk-config.nix")
       (
         { pkgs, ... }:
         {
           boot.kernelPackages = pkgs.linuxPackages_latest;
-          boot.kernelParams = lib.mkAfter [ "amdgpu.dcdebugmask=0x410" ];
-          # we only have one graphics card; no need for this
-          services.supergfxd.enable = false;
-          # this driver is buggy as heck # maybe not on 6.17?
-          # boot.blacklistedKernelModules = [ "hid_asus" ];
           hardware.bluetooth.enable = true;
         }
       )
