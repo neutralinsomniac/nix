843b250b85727b8c27972a6b1e5f0564eb51aa08
Jeremy O'Brien <neutral@fastmail.com>
2026-05-08 18:05:42 -0400

    leviathan: change to 1.5 scaling

--- hosts/leviathan.nix
+++ hosts/leviathan.nix
@@ -2,7 +2,7 @@
 {
   flake.nixosConfigurations.leviathan = lib.mkHost {
     name = "leviathan";
-    myHidpiScale = 1.75;
+    myHidpiScale = 1.5;
     extraModules = [
       inputs.nixos-hardware.nixosModules.asus-flow-gz302ea
       (inputs.self + "/hw/leviathan/disk-config.nix")
