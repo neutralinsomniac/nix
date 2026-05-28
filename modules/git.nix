dd7f6bf8faa3a750512b84c9e24a274e40ab00d3
Jeremy O'Brien <54ebac6c435033c3437ec400277e177d>
2026-05-27 22:49:04 -0400

    git: enable rngit signing

--- modules/git.nix
+++ modules/git.nix
@@ -3,9 +3,17 @@
     enable = true;
     config = [
       {
+        gpg = {
+          format = "ssh";
+          ssh.program = "rngcs";
+          ssh.allowedsignersfile = "none";
+        };
+      }
+      {
         user = {
           name = "Jeremy O'Brien";
           email = "neutral@fastmail.com";
+          signingKey = "~/.rngit/client_identity";
         };
       }
 
