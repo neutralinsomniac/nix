2eb41a204f015994e27e6a8271d0b212b1023f18
Jeremy O'Brien <neutral@fastmail.com>
2026-07-06 18:49:55 -0400

    use my personal nixos-hardware fork

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
     lanzaboote.url = "github:nix-community/lanzaboote/v1.0.0";
     lanzaboote.inputs.nixpkgs.follows = "nixpkgs";
 
-    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    nixos-hardware.url = "github:neutralinsomniac/nixos-hardware/master";
 
     nixos-apple-silicon.url = "github:nix-community/nixos-apple-silicon";
 
