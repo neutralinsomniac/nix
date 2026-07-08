beb04b7d1f7842537288d6c870fbf5ff549bba81
Jeremy O'Brien <neutral@fastmail.com>
2026-07-08 14:39:08 -0400

    docker.nix: remove broken options

--- modules/_docker.nix
+++ modules/_docker.nix
@@ -6,9 +6,4 @@
   environment.systemPackages = [ pkgs.docker-compose ];
   users.users.jeremy.extraGroups = [ "docker" ];
   virtualisation.docker.enable = true;
-  virtualisation.docker.storageDriver = "btrfs";
-  virtualisation.docker.rootless = {
-    enable = true;
-    setSocketVariable = true;
-  };
 }
