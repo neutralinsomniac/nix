f2e4cfb088e216cf1dbe4dc8a82c7bf79efb78ce
Jeremy O'Brien <neutral@fastmail.com>
2026-07-09 09:41:01 -0400

    reticulum: upgrade everything

--- overlays/reticulum.nix
+++ overlays/reticulum.nix
@@ -2,35 +2,29 @@ final: prev: {
   pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
     (pyfinal: pyprev: {
       rns = pyprev.rns.overridePythonAttrs rec {
-        version = "1.2.7";
-
-        src = final.fetchFromGitHub {
-          owner = "markqvist";
-          repo = "Reticulum";
-          tag = version;
-          hash = "sha256-Z4rAmprGj7MYkFHpaNwcMysjeSkLrqm115LzaNlI8I4=";
+        version = "1.3.7";
+        src = pyfinal.fetchPypi {
+          pname = "rns";
+          inherit version;
+          hash = "sha256-Z1fbZcCvISs4a35EuV7aTjbWsRyug0JYvG0tEdrG4SU=";
         };
       };
 
       lxmf = pyprev.lxmf.overridePythonAttrs rec {
-        version = "0.9.8";
-
-        src = final.fetchFromGitHub {
-          owner = "markqvist";
-          repo = "lxmf";
-          tag = version;
-          hash = "sha256-26T8f4WCf5q5/2RKA2Dh5xxqUOR3XXRFOzezCuDRA6c=";
+        version = "1.0.1";
+        src = pyfinal.fetchPypi {
+          pname = "lxmf";
+          inherit version;
+          hash = "sha256-0S6tRIKWy9CSA0YtjclqJutx7xaLQZexM6lw1IOmB+o=";
         };
       };
 
       nomadnet = pyprev.nomadnet.overridePythonAttrs (old: rec {
-        version = "1.1.0";
-
-        src = final.fetchFromGitHub {
-          owner = "markqvist";
-          repo = "NomadNet";
-          tag = version;
-          hash = "sha256-2XbEJfB9Qj58u3rdTQA4DY2ZsVk/6FBhvlggBdrwRBk=";
+        version = "1.2.6";
+        src = pyfinal.fetchPypi {
+          pname = "nomadnet";
+          inherit version;
+          hash = "sha256-XNRs4avq22JslxkSlqgTOj0bKuiVwam3i9bzoMAUjAU=";
         };
 
         dependencies = old.dependencies ++ [ pyfinal.msgpack ];
