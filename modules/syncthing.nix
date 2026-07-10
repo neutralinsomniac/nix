a85c008eea1e64ae7e207865fac5cb1a2fb94926
Jeremy O'Brien <neutral@fastmail.com>
2026-07-10 00:46:42 -0400

    syncthing: run as jeremy

--- modules/syncthing.nix
+++ modules/syncthing.nix
@@ -1,6 +1,9 @@
 {
   services.syncthing = {
     enable = true;
+    user = "jeremy";
+    dataDir = "/home/jeremy/Sync";
+    configDir = "/home/jeremy/.config/syncthing";
     openDefaultPorts = true; # Open ports in the firewall for Syncthing. (NOTE: this will not open syncthing gui port)
   };
 }
