1d828e31c6bb33b7af60e935495565d8ed6a050d
Jeremy O'Brien <neutral@fastmail.com>
2026-07-31 13:09:41 -0400

    sendai: work around slow fingerprint sensor wakeup

--- hosts/sendai.nix
+++ hosts/sendai.nix
@@ -10,6 +10,17 @@ lib.mkHost {
         boot.kernelPackages = pkgs.linuxPackages_latest;
         networking.networkmanager.wifi.powersave = false;
         services.fprintd.enable = true;
+        # the reader intermittently wedges after suspend; keep it out of USB
+        # autosuspend and stop fprintd before sleep so D-Bus activation
+        # restarts it cleanly on resume
+        # https://eldon.me/intermittent-fingerprint-reader-issue/
+        services.udev.extraRules = ''
+          ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="06cb", ATTR{idProduct}=="0123", ATTR{power/control}="on"
+        '';
+        systemd.services.fprintd = {
+          conflicts = [ "sleep.target" ];
+          before = [ "sleep.target" ];
+        };
         useSecureBoot = true;
       }
     )
